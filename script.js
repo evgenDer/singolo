91a4579c4ae50d263d2cab477f882b0eb109e907
Derevygo Evgeniya <evgeniya.nice@yandex.by>
2020-03-14 20:57:02 +0300

    feat: edit script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const MENU=document.getElementsById('menu');
+const MENU=document.getElementById('menu');
 
 
 MENU.querySelectorAll('li');
 
 MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('li').forEach(ul=> el.classList.remove('active'));
+    MENU.querySelectorAll('li').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
