4df900001d60802ef1d84e900142691995e1128d
Derevygo Evgeniya <evgeniya.nice@yandex.by>
2020-03-22 04:13:31 +0300

    feat: add scroll, add animation of slides

--- script.js
+++ script.js
@@ -5,129 +5,73 @@ const BUTTON=document.getElementById('btn');
 const BUTTON_CLOSE=document.getElementById('close_btn');
 const PHONE_VERT=document.getElementsByClassName('vert-iphone');
 const PHONE_HORIZ=document.getElementsByClassName('horiz-iphone');
-const SLIDER_BLOCK = document.getElementById('slider');
+const SLIDER_BLOCK = document.getElementById('home');
 const SLIDER_ARROW = document.getElementsByClassName('slider-block_arrows');
 const ANCHORS= document.querySelectorAll('a[href*="#"]')
 
+//change slides
 
-let items = document.querySelectorAll('.item');
-let currentItem = 0;
+let slides = document.querySelectorAll('.item');
+let currentSlide = 0;
 let isEnabled = true;
+console.log(slides);
 
-function changeCurrentItem(n) {
-    currentItem = (n + items.length) % items.length;
+function changeCurrentSlide(n) {
+    currentSlide = (n + slides.length) % slides.length;
 }
 
-function hideItem(direction) {
+function hideSlide(direction) {
     isEnabled = false;
-    items[currentItem].classList.add(direction);
-    items[currentItem].addEventListener('animationend', function() {
-        this.classList.remove('active', direction);
-    })
+    slides[currentSlide].classList.add(direction);
+    slides[currentSlide].addEventListener('animationend', function() {
+        this.classList.remove('slide_active', direction);
+    });
 }
 
-function showItem(direction) {
-    items[currentItem].classList.add('next', direction);
-    items[currentItem].addEventListener('animationend', function() {
+function showSlide(direction) {
+    slides[currentSlide].classList.add('next', direction);
+    slides[currentSlide].addEventListener('animationend', function() {
         this.classList.remove('next', direction);
-        this.classList.add('active');
+        console.log(this);
+        this.classList.add('slide_active');
+        if(!slides[currentSlide].classList.contains("slide_blue")){
+            SLIDER_ARROW[0].style.filter='grayscale(0%)';
+            SLIDER_ARROW[1].style.filter='grayscale(0%)';   
+        }
+         else {
+             SLIDER_ARROW[0].style.filter='grayscale(100%)';
+             SLIDER_ARROW[1].style.filter='grayscale(100%)';
+         }
         isEnabled = true;
-    })
+    });
+        
 }
 
-function previousItem(n) {
-    hideItem('to-right');
-    changeCurrentItem(n - 1);
-    showItem('from-left');
+function previousSlide(n) {
+    hideSlide('to-right');
+    changeCurrentSlide(n - 1);
+    showSlide('from-left');
+
 }
 
-function nextItem(n) {
-    hideItem('to-left');
-    changeCurrentItem(n + 1);
-    showItem('from-right');
+function nextSlide(n) {
+    hideSlide('to-left');
+    changeCurrentSlide(n + 1);
+    showSlide('from-right');
 }
 document.querySelector('.right-arrow').addEventListener('click', function() {
     if (isEnabled) {
-        previousItem(currentItem)
+        previousSlide(currentSlide)
     }
 });
 document.querySelector('.left-arrow').addEventListener('click', function() {
     if (isEnabled) {
-        nextItem(currentItem)
+        nextSlide(currentSlide)
     }
 });
 
+//change screen(go off)
 
-/*let slides = document.querySelectorAll('.slide');
-let currentSlide=0;
-let isEnabled=true;
-
-function changeCurrentSlide(n){
-    currentSlide=(n+slides.length)% slides.length;
-}
-
-function previousSlide(n){
-    hideSlide('to-right');
-    changeCurrentSlide(n-1);
-    showSlide('from-left');
-}
-
-function nextSlide(n){
-    hideSlide('to-left');
-    changeCurrentSlide(n+1);
-    showSlide('from-right');
-}
-
-function hideSlide(direction){
-    isEnabled=false;
-    slides[currentSlide].classList.add(direction);
-    slides[currentSlide].addEventListener('animationend',function(){
-        this.classList.remove('slide_active', direction);
-    })
-}
-
-function showSlide(direction){
-    slides[currentSlide].classList.add('next', direction);
-    slides[currentSlide].addEventListener('animationend', function(){
-        this.classList.remove('next', direction);
-        this.classList.add('slide_active');
-        isEnabled=true;
-    })
-}
-
-document.querySelector('.slider-block_arrows.left-arrow').addEventListener('click',function(){
-    if(isEnabled){
-        previousSlide(currentSlide);
-    }
-});
-
-document.querySelector('.slider-block_arrows.right-arrow').addEventListener('click',function(){
-    if(isEnabled){
-        nextSlide(currentSlide);
-    }
-});
-
-/*function changeSlide() {
-	let slides = document.querySelectorAll(".slide");
-	slides.forEach(slide => {
-        if (slide.classList.contains("display-none")) {
-            slide.classList.remove("display-none");
-		} else {
-            if(slide.classList.contains("slide_blue")){
-                SLIDER_ARROW[0].style.filter='grayscale(0%)';
-                SLIDER_ARROW[1].style.filter='grayscale(0%)';   
-                SLIDER_BLOCK.style.borderBottomColor = '#ea676b';
-            }
-            else {
-                SLIDER_ARROW[0].style.filter='grayscale(100%)';
-                SLIDER_ARROW[1].style.filter='grayscale(100%)';
-                SLIDER_BLOCK.style.borderBottomColor = '#42aaff';
-            }
-            slide.classList.add("display-none");
-    	}
-	});
-}
-*/
 function clickOnHorizPhone(){
     if(PHONE_HORIZ[0].classList.contains("display-none"))
     {
@@ -160,6 +104,8 @@ function clickOnHorizPhone(){
         
         }
     }
+
+//scroll
     
     for (let anchor of ANCHORS) {
       anchor.addEventListener('click', function (elem) {
@@ -236,8 +182,8 @@ BUTTON_PORTF.querySelectorAll('button');
 BUTTON_PORTF.addEventListener('click', (event) => {
     BUTTON_PORTF.querySelectorAll('button').forEach(el => el.classList.remove('button_active'));
     event.target.classList.add('button_active');
-    let items=POPRTFIMG.querySelectorAll('img');
-    console.log(items.length);
+    let slides=POPRTFIMG.querySelectorAll('img');
+    console.log(slides.length);
     let size=items.length;
     console.log(POPRTFIMG.length);
     //let randomChange;
